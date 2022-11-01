0744eeba55d3e09ecb42a804905eb5859e1cab53
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-01 23:53:25 +0100

    App's purpose description
     App's purpose description with style

---
+++ src/components/Layout/Header/MealsSummary.js
@@ -0,0 +1,14 @@
+import './MealsSummary.css';
+
+const MealsSummary = () => {
+    return (
+        <div className="meals-summary">
+            <h2>Delicious Food, Delivered To You</h2>
+            <p>Choose your favorite meal and enjoy a delicious lunch or dinner at home.</p>
+            <p>All our meals are prepared with high-quality ingrediens, by chefs for whom cooking is a passion. 
+            Meals are cooked just-in-time for you to enjoy a warm meal. </p>
+        </div>
+    );
+};
+
+export default MealsSummary;
