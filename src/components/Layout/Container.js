fc92d0cf3a45ffe372d4ef52724361cffe456fce
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-01 23:49:58 +0100

    Restructurization
    Restructurization and style change

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -1,8 +1,11 @@
 import styles from './Container.module.css';
+import AvailableMeals from '../Meals/AvailableMeals';
 
 const Container = () => {
     return (
-        <div className={`${styles.container}`}></div>
+        <div className={`${styles.container}`}>
+            <AvailableMeals></AvailableMeals>
+        </div>
     );
 };
 
