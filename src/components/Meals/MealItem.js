84a2479e5446fe42e2a646578c4150d79cd6880f
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-01 23:54:59 +0100

    Single Meal Item
    Single Meal Item + Style

---
+++ src/components/Meals/MealItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './MealItem.module.css';
+
+import Card from "../UI/Card";
+import AddMealForm from "../UI/AddMealForm";
+
+const Meal = (props) => {
+    const price = `€${props.price.toFixed(2)}`;
+
+    return (
+        <Card>
+            <div className={`${styles.meal}`}>
+                <h3>{props.name}</h3>
+                <p className={`${styles.description}`}>{props.description}</p>
+                <p className={`${styles.price}`}>{price}</p>   
+            </div>
+            <AddMealForm></AddMealForm>
+        </Card>
+    );
+};
+
+export default Meal;
