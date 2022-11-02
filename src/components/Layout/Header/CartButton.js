f801ae7d9c15a52b459b7cd357da54e1595496f2
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-02 22:51:01 +0100

    CartButton
    Cosmetics

--- src/components/Layout/Header/CartButton.js
+++ src/components/Layout/Header/CartButton.js
@@ -1,18 +1,21 @@
-import classes from './CartButton.module.css'
-import '../../fontello/css/fontello.css'
+import { useContext } from 'react';
+import classes from './CartButton.module.css';
+import CartContext from '../../../store/CartContext';
+import '../../fontello/css/fontello.css';
 
-const CartButton = () => {
+const CartButton = (props) => {
+    const cartCTX = useContext(CartContext);
 
-    const num = 5;
+    const numOfCartItems = cartCTX.items.reduce((currNum, item) => {return currNum + item.amount;}, 0);
 
     return (
-        <button className={`${classes['cart-button']}`}>
+        <button className={`${classes['cart-button']}`} onClick={props.onClick}>
             <div className={`${classes['font-basket']}`}>
                 <i className="icon-basket" style={{ fontSize: '20px' , marginRight: '10px'}} />
             </div>
             <span>Your Cart</span>
-            <div className={`${classes['items-number']}`}>
-                <label>{num}</label>
+            <div className={`${classes['cart-button']} ${classes['items-number']}`}>
+                <label>{numOfCartItems}</label>
             </div>
         </button>
     );
