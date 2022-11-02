44f159f85200a71a8a801647159baafde45ace15
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-02 19:05:23 +0100

    Meal-Form
    Custom Meal Form Redesign

---
+++ src/components/Meals/AddMealForm.js
@@ -0,0 +1,24 @@
+import styles from './AddMealForm.module.css';
+import Button from "../UI/Button";
+import Input from '../UI/Input';
+
+const AmountOfMeals = (props) => {
+    return (
+        <form className={`${styles['form']}`}>    
+             
+            <Input label='Amount' input={
+                {
+                    id: 'amount',
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1',
+                }
+            }/>                    
+            <Button></Button>
+        </form> 
+    );
+};
+
+export default AmountOfMeals;
