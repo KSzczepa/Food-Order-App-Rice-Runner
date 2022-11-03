c437757cf751752cf6c1414e415cae0aa1ec4653
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-03 18:33:55 +0100

    CartAnimation
    Cart button 'bump' animation

--- src/components/Layout/Header/CartButton.js
+++ src/components/Layout/Header/CartButton.js
@@ -1,15 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import classes from './CartButton.module.css';
 import CartContext from '../../../store/CartContext';
 import '../../fontello/css/fontello.css';
 
 const CartButton = (props) => {
     const cartCTX = useContext(CartContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const {items} = cartCTX;
 
     const numOfCartItems = cartCTX.items.reduce((currNum, item) => {return currNum + item.amount;}, 0);
 
+    const btnClasses = `${classes['cart-button']} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={`${classes['cart-button']}`} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <div className={`${classes['font-basket']}`}>
                 <i className="icon-basket" style={{ fontSize: '20px' , marginRight: '10px'}} />
             </div>
