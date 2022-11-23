846b8a19e5fb5b6542e913794289ff6b908bfc2e
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-11-23 23:04:34 +0100

    update users route

--- server/src/api/routes/users.js
+++ server/src/api/routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2');
-// import passwordHash from 'password-hash';
 const passwordHash = require('password-hash');
 
 
@@ -25,30 +24,65 @@ connection.connect((err) => {
 });
 
 
-router.post('/', (req, res, next) => {
-    const password = req.body.password;
-    const hashedPassword = passwordHash.generate(password);
+router.post('/:val', (req, res, next) => {
+    if (req.params.val === 'login') {
+        const password = req.body.password;
 
-    connection.query("SELECT * FROM user_data.users", (err, response, fields)=>{
-        if (err) {
-            console.log(err);
-        }
-        else {
-            const data = JSON.parse(JSON.stringify(response));
-            for (let i=0; i<data.length; i++) {
-                console.log('verify:', passwordHash.verify(password, data[0].password));
-            }
-            // const hashedpassword = data.password;
-            
-            if (passwordHash.verify(password, data[0].password)){
-                res.send({isRegistered: true, data: data});
+        connection.query("SELECT * FROM user_data.users", (err, response, fields)=>{
+            if (err) {
+                console.log(err);
             }
             else {
-                res.send({isRegistered: false, data: null});
+                const data = JSON.parse(JSON.stringify(response));
+                for (let i=0; i<data.length; i++) {
+                    console.log('verify:', passwordHash.verify(password, data[i].password));
+                }
+                
+                if (passwordHash.verify(password, data[0].password)){
+                    res.send({isRegistered: true, data: data[0]});
+                }
+                else {
+                    res.send({isRegistered: false, data: null});
+                }
+                
             }
-            
-        }
-    });
+        });
+    }
+    else if (req.params === 'sign') {
+
+        const userData = {
+            ID: req.body.ID,
+            login: req.body.login,
+            password: req.body.password,
+            email: req.body.email,
+            name: req.body.name,
+            street: req.body.street,
+            city: req.body.city,
+            postalCode: req.body.postalCode,
+            phoneNumber: req.body.phoneNumber,
+        };
+        //first we should validate again user's data
+        //check if threre is no user registered with the same;
+        //ID, login, password, email or phone number
+
+        connection.query("SELECT * FROM user_data.users", (err, response, fields)=>{
+            if (err) {
+                console.log(err);
+            }
+            else {
+                const data = JSON.parse(JSON.stringify(response));
+                for (let i=0; i<data.length; i++) {
+                    if (data[i].login || data[i].password || data[i].email || data[i].phoneNumber) {
+                        res.send({message: "Error"});
+                    }
+                }
+                
+                
+                
+            }
+        });
+
+    }
 
 });
 
