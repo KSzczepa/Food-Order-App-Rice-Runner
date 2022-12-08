751cfdd5545c8808c8a24f15e0a971f4df155074
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-12-08 21:46:50 +0100

    Modal.js update

--- api/src/components/UI/Modal.js
+++ api/src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-    return (<div className={classes.backdrop} onClick={props.onCloseCart}></div>);
+    return (<div className={classes.backdrop} onClick={props.onClose}></div>);
 };
 
 const ModalOverlay = (props) => {
@@ -19,7 +19,7 @@ const portal = document.getElementById('overlays');
 const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop onCloseCart={props.onCloseCart}/>, portal)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portal)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portal)}
         </Fragment>
     );
