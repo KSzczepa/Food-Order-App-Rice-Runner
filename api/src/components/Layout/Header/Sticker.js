7f5c3f869267688e8cb07947e4d1d201300ae206
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-12-09 18:56:26 +0100

    Sticker Component Update

--- api/src/components/Layout/Header/Sticker.js
+++ api/src/components/Layout/Header/Sticker.js
@@ -1,12 +1,26 @@
+import React, {useContext} from 'react';
 import classes from './Sticker.module.css';
 import CartButton from './CartButton';
+import AuthContext from '../../../store/AuthContext/auth-context';
+import '../../fontello-auth/css/fontello.css';
+import UserLogButton from './UserLogButton';
 
 const Sticker = (props) => {
+    const authCtx = useContext(AuthContext);
 
+    // const showUserLogginForm = () => {
+    //     console.log('LogginFormActive?');
+    //     authCtx.loginFormActive = true;
+    //     console.log(authCtx.loginFormActive);
+    // };
 
     return (
         <div className={`${classes.header}`}>
-            <div className={`${classes.sticker}`}>
+            <div className={`${classes.sticker}`}>                
+                <div className={`${classes['font-basket']}`}>
+                    {/* {!authCtx.isLoggedIn && */}
+                    <UserLogButton onClick={props.onShowUserForm}></UserLogButton>
+                </div>
                 <h2>ReactMeals</h2>
                 <CartButton onClick={props.onShowCart}></CartButton>
             </div>
