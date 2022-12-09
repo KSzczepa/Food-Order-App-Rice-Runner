9de6eb78e43d11c2ea19b22459ab8b201f521f58
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-12-09 18:57:39 +0100

    App.js update
    Authorization added

--- api/src/App.js
+++ api/src/App.js
@@ -1,14 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 
 import Sticker from "./components/Layout/Header/Sticker";
 import Header from "./components/Layout/Header/Header";
 import Container from "./components/Layout/Container/Container";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import AuthContext from "./store/AuthContext/auth-context";
+import Card from "./components/UI/Card";
+import LogginForm from "./components/LogginForm/LogginForm";
 
 function App() {
 
+	const ctx = useContext(AuthContext);
+
 	const [cartButtonVisible, setCartButtonVisible] = useState(false);
+	const [userLogginFormVisible, setUserLogginFormVisible] = useState(false);
 
 	const showCartHandler = () => {
 		setCartButtonVisible(true);
@@ -18,10 +24,27 @@ function App() {
 		setCartButtonVisible(false);
 	};
 
+	console.log('loggedIn', ctx.isLoggedIn);
+	console.log('formActive', ctx.loginFormActive);
+
+	const showUserFormHandler = () => {
+		console.log('LogginFormActive?');
+        ctx.loginFormActive = true;
+		setUserLogginFormVisible(true);
+		// ctx.onSetLoggingForm();
+	};
+
+	const hideUserFormHandler = () => {
+		// ctx.onCloseLogForm();
+		ctx.loginFormActive = false;
+		setUserLogginFormVisible(false);
+	};
+
 	return (
 		<CartProvider>
+			{ctx.loginFormActive &&<LogginForm onClickLogForm={hideUserFormHandler}/>}
 			{cartButtonVisible && <Cart onCloseCart={hideCartHandler}/>}
-			<Sticker onShowCart={showCartHandler}></Sticker>
+			<Sticker onShowCart={showCartHandler} onShowUserForm={showUserFormHandler}></Sticker>
 			<main>				
 				<Header></Header>
 				<Container></Container>
