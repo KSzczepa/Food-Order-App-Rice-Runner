9fdfc403ff38e351dc7c03eb71186f20cfe2f844
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-01-21 16:25:03 +0100

    Update index.js
    Store with authorization context provided

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './store/AuthContext/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
