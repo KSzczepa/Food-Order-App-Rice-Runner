a9f3c9da427c5676bd8f5d1790fa5b377a438fa0
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-01-22 22:32:22 +0100

    Main form
    style changes

--- api/src/components/LogginForm/LogginMainForm.js
+++ api/src/components/LogginForm/LogginMainForm.js
@@ -1,19 +1,19 @@
 import Modal from "../UI/Modal";
 import UserAuthForm from "./UserAuthForm";
 import styles from './LogginMainForm.module.css';
-import Card from "../UI/Card";
+import CardAuth from "../UI/CardAuth";
 
 const LogginForm = (props) => {
 
-    return (<Modal onClose={props.onClickLogForm}>
-        <Card>
+    return (<Modal onClose={props.onClickLogForm} style={{width: '26rem', left: 'calc(50% - 13rem)'}}>
+        <CardAuth>
         <UserAuthForm/>
         <div className={styles.sign}>
             {/* <p>Don't have account?</p> */}
             <span>Don't have account?</span>
             <button className={styles.btn}>Sign in</button>
         </div>
-        </Card>
+        </CardAuth>
     </Modal>)
 };
 
