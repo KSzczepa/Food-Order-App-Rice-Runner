8b915eed53c1f70c9c92599c85cd4bb00d4109d4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-01-22 22:32:56 +0100

    Main App
    authentication mode with redux

--- api/src/App.js
+++ api/src/App.js
@@ -1,20 +1,21 @@
-import React, {useState, useContext} from "react";
+import React, {useState} from "react";
 
 import Sticker from "./components/Layout/Header/Sticker";
 import Header from "./components/Layout/Header/Header";
 import Container from "./components/Layout/Container/Container";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
-import AuthContext from "./store/AuthContext/auth-context";
-import Card from "./components/UI/Card";
 import LogginForm from "./components/LogginForm/LogginMainForm";
+import {useSelector, useDispatch} from 'react-redux';
+import { authActions } from './store/AuthContext/auth';
 
 function App() {
 
-	const ctx = useContext(AuthContext);
+	const dispatch = useDispatch();
+	const isAuth = useSelector(state => state.auth.isAuthenticated);
+	const isLogFormActive = useSelector(state => state.auth.isLoginFormActive);
 
 	const [cartButtonVisible, setCartButtonVisible] = useState(false);
-	const [userLogginFormVisible, setUserLogginFormVisible] = useState(false);
 
 	const showCartHandler = () => {
 		setCartButtonVisible(true);
@@ -24,25 +25,17 @@ function App() {
 		setCartButtonVisible(false);
 	};
 
-	console.log('loggedIn', ctx.isLoggedIn);
-	console.log('formActive', ctx.loginFormActive);
-
 	const showUserFormHandler = () => {
-		console.log('LogginFormActive?');
-        ctx.loginFormActive = true;
-		setUserLogginFormVisible(true);
-		// ctx.onSetLoggingForm();
+		dispatch(authActions.showLoginForm());
 	};
 
 	const hideUserFormHandler = () => {
-		// ctx.onCloseLogForm();
-		ctx.loginFormActive = false;
-		setUserLogginFormVisible(false);
+		dispatch(authActions.hideLoginForm());
 	};
 
 	return (
 		<CartProvider>
-			{ctx.loginFormActive &&<LogginForm onClickLogForm={hideUserFormHandler}/>}
+			{isLogFormActive &&<LogginForm onClickLogForm={hideUserFormHandler}/>}
 			{cartButtonVisible && <Cart onCloseCart={hideCartHandler}/>}
 			<Sticker onShowCart={showCartHandler} onShowUserForm={showUserFormHandler}></Sticker>
 			<main>				
