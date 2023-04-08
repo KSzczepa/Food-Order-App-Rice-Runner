9ce76500cb79e79a79c9cce644fc1c1d7c68d5b9
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-04-08 21:11:33 +0200

    sticker update
    new app name

--- api/src/components/Layout/Header/Sticker.js
+++ api/src/components/Layout/Header/Sticker.js
@@ -19,8 +19,8 @@ const Sticker = (props) => {
                     {/* {!authCtx.isLoggedIn && */}
                     <UserLogButton onClick={props.onShowUserForm}></UserLogButton>
                 </div>
-                <h2 className={classes.desktop}>ReactMeals</h2>
-                <h2 className={classes.mobile}>RM</h2>
+                <h2 className={classes.desktop}>RiceRunner</h2>
+                <h2 className={classes.mobile}>RR</h2>
                 <CartButton onClick={props.onShowCart}></CartButton>
             </div>
             
