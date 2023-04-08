55e554958884c810c7b246f11e03fe1cbfed8c72
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-04-08 21:11:17 +0200

    image path update

--- api/src/components/Layout/Header/Image.js
+++ api/src/components/Layout/Header/Image.js
@@ -5,7 +5,8 @@ const Image = () => {
 
     return (
         <div className='img-trunk'>
-            <img className='main-img' src={foodImg} alt='A table full of delicious food.'></img>
+            {/* <img className='main-img' src={foodImg} alt='A table full of delicious food.'></img> */}
+            <img className='main-img' src={process.env.PUBLIC_URL + '/img/food15.jpg'} alt='A table full of delicious food.'></img>
         </div>
     );
 };
