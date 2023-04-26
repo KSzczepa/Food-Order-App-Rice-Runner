431e5c858e068beb67d0c174ebf36dd822c25fdd
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-04-26 17:08:57 +0200

    app configuration update

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-const functions = require('firebase-functions');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
 
 const mongoConnect = require('./api/util/database').mongoConnect;
 
-const menuRoutes = require('./api/routes/firebase/menu');
+const menuRoutes = require('./api/routes/menuRoute');
+const orderRoutes = require('./api/routes/orderRoute');
 
-// app.use(cors);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
@@ -28,16 +24,14 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use('/', (req, res) => {console.log('Hakuna matata'); res.send('Hakuna matata');});
 app.use('/menu', menuRoutes);
-
+app.use('/orders', orderRoutes);
 
 // mongoose.connect('mongodb+srv://JustUser:xO8ijc3PL7TtBmeo@cluster0.iun9qhb.mongodb.net/RiceRunnerDatabase?retryWrites=true&w=majority')
 //     .then(() => console.log('Connected to mongoDB with mongoose'));
 
 mongoConnect((client) => {
-    // app.listen(6000);
-    console.log('MongoDB server is running!');
+    // console.log('MongoDB server is running!');
   });
 
 //previous version
